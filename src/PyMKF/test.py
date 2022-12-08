779ed769f1d6acc713d06d43153ec6110a96e779
AlfVII <Alfonso_VII@hotmail.com>
2022-12-08 20:56:34 +0100

    Added reluctances models and tests

--- src/PyMKF/test.py
+++ src/PyMKF/test.py
@@ -2,6 +2,34 @@ import pprint
 import PyMKF
 
 
-constants = PyMKF.Constants()
-pprint.pprint(constants.residual_gap)
+constants = PyMKF.get_constants()
+pprint.pprint(constants['residualGap'])
+pprint.pprint(constants['minimumNonResidualGap'])
 pprint.pprint(PyMKF.get_constants())
+
+
+coreData = {
+    'functionalDescription': {
+        'bobbin': None,
+        'gapping': [],
+        'material': '3C97',
+        'name': 'default',
+        'numberStacks': 1,
+        'shape': {'aliases': [],
+                  'dimensions': {'A': 0.0308,
+                                 'B': 0.0264,
+                                 'C': 0.0265,
+                                 'D': 0.016,
+                                 'E': 0.01,
+                                 'G': 0.0,
+                                 'H': 0.0},
+                  'family': 'u',
+                  'familySubtype': '1',
+                  'name': 'Custom',
+                  'type': 'custom'},
+        'type': 'two-piece set'
+    }
+}
+
+core_data = PyMKF.get_core_data(coreData)
+pprint.pprint(core_data)
--- src/Reluctance.cpp
+++ src/Reluctance.cpp
@@ -6,51 +6,160 @@
 #include <filesystem>
 #include <streambuf>
 #include <magic_enum.hpp>
-
 #include "Reluctance.h"
 
 
 namespace OpenMagnetics {
 
-    class ZhangModel: public Reluctance
+    std::map<std::string, double> ReluctanceZhangModel::get_gap_reluctance(CoreGap gapInfo)
     {
-        // Based on Improved Calculation Method for Inductance Value of the Air-Gap Inductor by Xinsheng Zhang
-        // https://sci-hub.wf/https://ieeexplore.ieee.org/document/9332553
-        public:
-            std::map<std::string, double> get_gap_reluctance(CoreGap gapInfo)
-            {
-                double perimeter = 0;
-                double gap_length = gapInfo.get_length();
-                double gap_area = *(gapInfo.get_area());
-                double gap_shape = *(gapInfo.get_shape());
-                double gap_section_dimensions = *(gapInfo.get_section_dimensions());
-                double distance_closest_normal_surface = *(gapInfo.get_distance_closest_normal_surface());
-                double reluctance_internal = gap_length / (constants.vacuum_permeability * gap_area);
-                double reluctance_fringing = 0;
-                double fringing_factor = 1;
-                double gap_section_width = gap_section_dimensions[0];
-                double gap_section_depth = gap_section_dimensions[1];
+        double perimeter = 0;
+        auto constants = Constants();
+        auto gap_length = gapInfo.get_length();
+        auto gap_area = *(gapInfo.get_area());
+        auto gap_shape = *(gapInfo.get_shape());
+        auto gap_section_dimensions = *(gapInfo.get_section_dimensions());
+        auto distance_closest_normal_surface = *(gapInfo.get_distance_closest_normal_surface());
+        auto reluctance_internal = gap_length / (constants.vacuum_permeability * gap_area);
+        double reluctance_fringing = 0;
+        double fringing_factor = 1;
+        auto gap_section_width = gap_section_dimensions[0];
+        auto gap_section_depth = gap_section_dimensions[1];
 
-                if (gap_shape == ShapeEnum::ROUND) {
-                    perimeter = std::numbers::pi * gap_section_width;
-                }
-                else { // TODO: Properly calcualte perimeter for all shapes
-                    perimeter = column_width * 2 + gap_section_depth * 2;
-                }
+        if (gap_shape == ShapeEnum::ROUND) {
+            perimeter = std::numbers::pi * gap_section_width;
+        }
+        else { // TODO: Properly calcualte perimeter for all shapes
+            perimeter = gap_section_width * 2 + gap_section_depth * 2;
+        }
 
-                if (gap_length > 0) {
-                    reluctance_fringing = std::numbers::pi / (constants.vacuum_permeability * perimeter * log((distance_closest_normal_surface + gap_length) / gap_length));
-                }
+        if (gap_length > 0) {
+            reluctance_fringing = std::numbers::pi / (constants.vacuum_permeability * perimeter * log((2* distance_closest_normal_surface + gap_length) / gap_length));
+        }
 
-                double reluctance = 1 / (1 / reluctance_internal + 1 / reluctance_fringing);
+        double reluctance = 1. / (1. / reluctance_internal + 1. / reluctance_fringing);
 
-                if (gap_length > 0) {
-                    fringing_factor = gap_length / (constants.vacuum_permeability * gap_area * reluctance)
-                }
-                std::map<std::string, double> result;
-                result["reluctance"] = reluctance
-                result["fringing_factor"] = fringing_factor
+        if (gap_length > 0) {
+            fringing_factor = gap_length / (constants.vacuum_permeability * gap_area * reluctance);
+        }
+        std::map<std::string, double> result;
+        result["reluctance"] = reluctance;
+        result["fringing_factor"] = fringing_factor;
 
-                return result;
-            }
+        return result;
     };
+
+    std::map<std::string, double> ReluctanceMuehlethalerModel::get_gap_reluctance(CoreGap gapInfo)
+    {
+        auto constants = Constants();
+        auto gap_length = gapInfo.get_length();
+        auto gap_area = *(gapInfo.get_area());
+        auto gap_shape = *(gapInfo.get_shape());
+        auto gap_section_dimensions = *(gapInfo.get_section_dimensions());
+        auto distance_closest_normal_surface = *(gapInfo.get_distance_closest_normal_surface());
+        double reluctance_no_fringing = gap_length / (constants.vacuum_permeability * gap_area);
+        double reluctance;
+        double fringing_factor = 1;
+        auto gap_section_width = gap_section_dimensions[0];
+        auto gap_section_depth = gap_section_dimensions[1];
+
+        if (gap_shape == ShapeEnum::ROUND) {
+            double gamma_r = get_reluctance_type_1(gap_length / 2, gap_section_width / 2, distance_closest_normal_surface) / (gap_length / constants.vacuum_permeability / (gap_section_width / 2));
+            reluctance = pow(gamma_r, 2) * gap_length / (constants.vacuum_permeability * std::numbers::pi * pow(gap_section_width / 2, 2));
+        }
+        else {
+            double gamma_x = get_reluctance_type_1(gap_length / 2, gap_section_width, distance_closest_normal_surface) / (gap_length / constants.vacuum_permeability / gap_section_width);
+            double gamma_y = get_reluctance_type_1(gap_length / 2, gap_section_depth, distance_closest_normal_surface) / (gap_length / constants.vacuum_permeability / gap_section_depth);
+            double gamma = gamma_x * gamma_y;
+            reluctance = gamma * gap_length / (constants.vacuum_permeability * gap_section_depth * gap_section_width);
+        }
+
+        if (gap_length > 0) {
+            fringing_factor = std::max(1., reluctance_no_fringing / reluctance);
+        }
+
+        std::map<std::string, double> result;
+        result["reluctance"] = reluctance;
+        result["fringing_factor"] = fringing_factor;
+
+        return result;
+    };
+
+    std::map<std::string, double> ReluctanceEffectiveAreaModel::get_gap_reluctance(CoreGap gapInfo)
+    {
+        auto constants = Constants();
+        auto gap_length = gapInfo.get_length();
+        auto gap_area = *(gapInfo.get_area());
+        auto gap_shape = *(gapInfo.get_shape());
+        auto gap_section_dimensions = *(gapInfo.get_section_dimensions());
+        double reluctance;
+        double fringing_factor = 1;
+        auto gap_section_width = gap_section_dimensions[0];
+        auto gap_section_depth = gap_section_dimensions[1];
+
+        if (gap_length > 0) {
+            if (gap_shape == ShapeEnum::ROUND) {
+                fringing_factor = pow(1 + gap_length / gap_section_width, 2);
+            }
+            else{
+                fringing_factor = (gap_section_depth + gap_length) * (gap_section_width + gap_length) / (gap_section_depth * gap_section_width);
+            }
+        }
+
+        reluctance = gap_length / (constants.vacuum_permeability * gap_area * fringing_factor);
+
+        std::map<std::string, double> result;
+        result["reluctance"] = reluctance;
+        result["fringing_factor"] = fringing_factor;
+
+        return result;
+    };
+
+
+    std::map<std::string, double> ReluctanceMcLymanModel::get_gap_reluctance(CoreGap gapInfo)
+    {
+        auto constants = Constants();
+        auto gap_length = gapInfo.get_length();
+        auto gap_area = *(gapInfo.get_area());
+        auto gap_section_dimensions = *(gapInfo.get_section_dimensions());
+        auto distance_closest_normal_surface = *(gapInfo.get_distance_closest_normal_surface());
+        double reluctance;
+        double fringing_factor = 1;
+        auto gap_section_depth = gap_section_dimensions[1];
+
+        if (gap_length > 0) {
+            fringing_factor = 1 + (1.1 * gap_length / gap_section_depth) * log(2 * 2 * distance_closest_normal_surface / gap_length);
+        }
+
+        reluctance = gap_length / (constants.vacuum_permeability * gap_area * fringing_factor);
+
+        std::map<std::string, double> result;
+        result["reluctance"] = reluctance;
+        result["fringing_factor"] = fringing_factor;
+
+        return result;
+    };
+
+
+    std::shared_ptr<ReluctanceModel> ReluctanceModel::factory(ReluctanceModels modelName)
+    {
+        if (modelName == ReluctanceModels::ZHANG) {
+            std::shared_ptr<ReluctanceModel> reluctanceModel(new ReluctanceZhangModel);
+            return reluctanceModel;
+        }
+        else if (modelName == ReluctanceModels::MCLYMAN) {
+            std::shared_ptr<ReluctanceModel> reluctanceModel(new ReluctanceMcLymanModel);
+            return reluctanceModel;
+        }
+        else if (modelName == ReluctanceModels::EFFECTIVE_AREA) {
+            std::shared_ptr<ReluctanceModel> reluctanceModel(new ReluctanceEffectiveAreaModel);
+            return reluctanceModel;
+        }
+        else if (modelName == ReluctanceModels::MUEHLETHALER) {
+            std::shared_ptr<ReluctanceModel> reluctanceModel(new ReluctanceMuehlethalerModel);
+            return reluctanceModel;
+        }
+
+        else throw std::runtime_error("Unknown Reluctance mode, available options are: {ZHANG, MCLYMAN, EFFECTIVE_AREA, MUEHLETHALER}");
+    }
+}
