37c35834f73d80bb3ea6ea0d5863f2da02941681
AlfVII <Alfonso_VII@hotmail.com>
2024-02-11 01:24:06 +0100

    Added support for leakage inductance from any winding to any winding

--- src/LeakageInductance.cpp
+++ src/LeakageInductance.cpp
@@ -66,63 +66,6 @@ ComplexField LeakageInductance::calculate_magnetic_field(OperatingPoint operatin
     return field;
 }
 
-LeakageInductanceOutput LeakageInductance::calculate_leakage_inductance(OperatingPoint operatingPoint, MagneticWrapper magnetic, size_t sourceIndex, size_t destinationIndex, size_t harmonicIndex) {
-    auto settings = OpenMagnetics::Settings::GetInstance();
-    auto numberPointsX = settings->get_magnetic_field_number_points_x();
-    auto numberPointsY = settings->get_magnetic_field_number_points_y();
-    auto originallyIncludeFringing = settings->get_magnetic_field_include_fringing();
-    settings->set_magnetic_field_include_fringing(false);
-
-    auto bobbin = magnetic.get_mutable_coil().resolve_bobbin();
-    if (!bobbin.get_processed_description()){
-        throw std::runtime_error("Bobbin is not processed");
-    }
-    if (!bobbin.get_processed_description()->get_column_width()){
-        throw std::runtime_error("Bobbin is missing column width");
-    }
-    double bobbinColumnWidth = bobbin.get_processed_description()->get_column_width().value();
-    double bobbinColumnDepth = bobbin.get_processed_description()->get_column_depth();
-    auto coreColumnShape = magnetic.get_mutable_core().get_columns()[0].get_shape();
-    double coreWindingWindowWidth = magnetic.get_mutable_core().get_winding_windows()[0].get_width().value();
-    double coreWindingWindowHeight = magnetic.get_mutable_core().get_winding_windows()[0].get_height().value();
-
-    double currentRms = operatingPoint.get_excitations_per_winding()[0].get_current()->get_processed()->get_rms().value();
-
-    ComplexField field = calculate_magnetic_field(operatingPoint, magnetic, harmonicIndex);
-
-    double dx = coreWindingWindowWidth / numberPointsX;
-    double dy = coreWindingWindowHeight / numberPointsY;
-    double dA = dx * dy;
-    double vacuumPermeability = Constants().vacuumPermeability;
-
-    double energy = 0;
-    for (auto datum : field.get_data()){
-        double length;
-
-        if (coreColumnShape == ColumnShape::ROUND) {
-            length = 2 * std::numbers::pi * datum.get_point()[0];
-        }
-        else {
-            length = 2 * std::numbers::pi * (datum.get_point()[0] - bobbinColumnWidth) + 2 * 2 * bobbinColumnWidth + 2 * 2 * bobbinColumnDepth;
-        }
-        double HeSquared = pow(datum.get_real(), 2) + pow(datum.get_imaginary(), 2);
-        energy += 0.5 * vacuumPermeability * HeSquared * dA * length;
-    }
-
-    double leakageInductance = 2.0 / pow(currentRms, 2) * energy;
-    LeakageInductanceOutput leakageInductanceOutput;
-
-    leakageInductanceOutput.set_method_used("Energy");
-    leakageInductanceOutput.set_origin(ResultOrigin::SIMULATION);
-    DimensionWithTolerance dimensionWithTolerance;
-    dimensionWithTolerance.set_nominal(leakageInductance);
-    leakageInductanceOutput.set_leakage_inductance_per_winding({dimensionWithTolerance});
-
-    settings->set_magnetic_field_include_fringing(originallyIncludeFringing);
-
-    return leakageInductanceOutput;
-}
-
 LeakageInductanceOutput LeakageInductance::calculate_leakage_inductance(MagneticWrapper magnetic, double frequency, size_t sourceIndex, size_t destinationIndex, size_t harmonicIndex) {
     auto settings = OpenMagnetics::Settings::GetInstance();
     auto numberPointsX = settings->get_magnetic_field_number_points_x();
--- src/LeakageInductance.h
+++ src/LeakageInductance.h
@@ -14,7 +14,6 @@ class LeakageInductance{
         virtual ~LeakageInductance() = default;
 
 
-    LeakageInductanceOutput calculate_leakage_inductance(OperatingPoint operatingPoint, MagneticWrapper magnetic, size_t sourceIndex = 0, size_t destinationIndex = 1, size_t harmonicIndex = 1);
     LeakageInductanceOutput calculate_leakage_inductance(MagneticWrapper magnetic, double frequency, size_t sourceIndex = 0, size_t destinationIndex = 1, size_t harmonicIndex = 1);
     ComplexField calculate_magnetic_field(OperatingPoint operatingPoint, MagneticWrapper magnetic, size_t sourceIndex = 0, size_t destinationIndex = 1, size_t harmonicIndex = 1);
 
