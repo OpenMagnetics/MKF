c8e987f8e6aa37ee90d834c9b4943eaf909fbb93
AlfVII <Alfonso_VII@hotmail.com>
2024-09-02 20:34:31 +0200

    Added method to get advised sections

--- src/CoilAdviser.h
+++ src/CoilAdviser.h
@@ -16,6 +16,7 @@ class CoilAdviser : public WireAdviser {
 
         std::vector<MasWrapper> get_advised_coil(MasWrapper mas, size_t maximumNumberResults=1);
         std::vector<MasWrapper> get_advised_coil(std::vector<WireWrapper>* wires, MasWrapper mas, size_t maximumNumberResults=1);
+        std::vector<Section> get_advised_sections(MasWrapper mas, std::vector<size_t> pattern, size_t repetitions);
         std::vector<MasWrapper> get_advised_coil_for_pattern(std::vector<WireWrapper>* wires, MasWrapper mas, std::vector<size_t> pattern, size_t repetitions, std::vector<WireSolidInsulationRequirements> solidInsulationRequirementsForWires, size_t maximumNumberResults, std::string reference);
         std::vector<std::vector<WireSolidInsulationRequirements>> get_solid_insulation_requirements_for_wires(InputsWrapper& inputs, std::vector<size_t> pattern, size_t repetitions);
         void set_allow_margin_tape(bool value) {
