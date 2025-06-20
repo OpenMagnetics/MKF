1e1df3238bf3bfa4cfec87ebda7d6e5dd5c0cc52
AlfVII <Alfonso_VII@hotmail.com>
2025-06-20 14:02:13 +0200

    Added method to get core losses equations

--- src/physical_models/CoreLosses.h
+++ src/physical_models/CoreLosses.h
@@ -451,6 +451,7 @@ class CoreLossesProprietaryModel : public CoreLossesSteinmetzModel {
                                                                         double coreLosses) {
         return _get_magnetic_flux_density_from_core_losses(core, frequency, temperature, coreLosses);
     }
+    std::map<std::string, std::string> get_core_volumetric_losses_equations(CoreMaterial coreMaterial);
 
 };
 
